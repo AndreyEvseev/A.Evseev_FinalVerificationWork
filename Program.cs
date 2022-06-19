e3dade83326d99fa51bfff11f45ca6a406585a46
Andrey Evseev <a-evseev@yandex.ru>
2022-06-19 17:25:02 +0300

    Скорректирована блок-схема алгоритма

--- Program.cs
+++ Program.cs
@@ -1,6 +1,70 @@
-﻿
+﻿string[] CreateStringArray(int size = 5)
+{
+    return new string[size];
+}
 
+void FillStringArrayConsole(string [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"Введите {i}-й элемент массива строк: ");
+        array[i] = Console.ReadLine();
+        Console.WriteLine();
+    }
+}
 
+int DeterminingLengthResultArray(string[] array, int value)
+{
+    int lenResult = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= value) lenResult ++;
+    }
+    return lenResult;
+}
 
+void FillResultArray(string [] array, string [] result, int value)
+{
+    int j = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= value)
+        {
+            result[j] = array[i];
+            j ++;
+        }
+    }
+}
 
+void OutputArrayConsole(string [] array)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{(char)34}{array[i]}{(char)34}, ");
+    }
+    Console.WriteLine("]");
+}
 
+string OutputArrayString(string [] array)
+{
+    string result = string.Empty;
+    result = "[ ";
+    for (int i = 0; i < array.Length; i++)
+    {
+        result += $"{array[i]} ";
+    }
+    result += "]";
+    return result;
+}
+
+string[] received = CreateStringArray(6);
+FillStringArrayConsole(received);
+OutputArrayConsole(received);
+int value = 3;
+int lenResult = DeterminingLengthResultArray(received, value);
+Console.WriteLine(lenResult);
+string[] result = CreateStringArray(lenResult);
+FillResultArray(received, result, value);
+OutputArrayConsole(result);
+string resultString = OutputArrayString(result);
